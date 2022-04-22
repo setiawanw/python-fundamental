31392f491b32d00a1a913e5ea969d5732372b89f
setiawanw <wildannamora@outlook.com>
2022-04-22 08:31:18 +0700

    tipe data list part 2 dan comprehension part 1

--- dasar_sintaksis_tipe_data_list.py
+++ dasar_sintaksis_tipe_data_list.py
@@ -62,3 +62,34 @@ daftar_buku = ['Seven Habit', 'How to Influence People', 'First Things First', '
 daftar_buku.pop(-1) # -1 berarti buku keempat '40X' akan hilang, -1 itu dihitung dari belakang
 for i in range(0, len(daftar_buku)):
     print(daftar_buku[i])
+
+# masuk materi Tipe Data List Comprehension (sudah di copas di module dasar_sintaksis_tipe_data_listcomprehension):
+print('\nPerintah del')
+daftar_buku = ['Seven Habit', 'How to Influence People', 'First Things First', '40X']
+del daftar_buku[0] #0 berarti buku ke 1 'Seven Habit' akan hilang
+for i in range(0, len(daftar_buku)):
+    print(daftar_buku[i])
+
+print('\nPerintah del dengan list comprehension')
+daftar_buku = ['Seven Habit', 'How to Influence People', 'First Things First', '40X']
+del daftar_buku[:] # [:] berarti hapus semua elemen -- aslinya [start:stop}
+for i in range(0, len(daftar_buku)):
+    print(daftar_buku[i])
+
+#print('\nPerintah del dengan list comprehension start:stop')
+#daftar_buku = ['Seven Habit', 'How to Influence People', 'First Things First', '40X']
+#del daftar_buku[0:3] # [0:3] hapus dari index 0 'Seven Habit', dan hapus sejumah 3 elemen, jadi nanti sisa 40X (index itu dihitung dari 0, sementara jumlah itu dari 1)
+#for i in range(0, len(daftar_buku)):
+#    print(daftar_buku[i])
+
+#Coba ini:
+#print('\nPerintah del dengan list comprehension start:stop')
+#daftar_buku = ['Seven Habit', 'How to Influence People', 'First Things First', '40X']
+#del daftar_buku[0:-2]
+#for i in range(0, len(daftar_buku)):
+#    print(daftar_buku[i])
+
+print('\nPerintah del dengan list comprehension start:stop')
+daftar_buku = ['Seven Habit', 'How to Influence People', 'First Things First', '40X']
+del daftar_buku[0::2] #yang dihapus bisa di jeda2 -- slicing datafor i in range(0, len(daftar_buku)):
+    print(daftar_buku[i])
